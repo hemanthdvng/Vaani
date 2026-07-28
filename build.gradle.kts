1b6b7098cf3b684d20a81f064ef7dadac4e27fa7
hemanthdvng <hemanthdvng1@gmail.com>
2026-07-28 11:42:24 +0530

    Vaani: project skeleton (call screening + Room + LiteRT-LM)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    // No kotlin-android plugin: AGP 9.0+ provides built-in Kotlin support.
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
