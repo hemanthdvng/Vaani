204e554b3757e10bb1f0ec85b1edf09414a78640
hemanthdvng <hemanthdvng1@gmail.com>
2026-07-28 11:36:28 +0530

    Vaani: project skeleton (call screening + Room + LiteRT-LM)

---
+++ app/src/main/java/com/hemanth/vaani/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.hemanth.vaani.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark terminal aesthetic: near-black surfaces, phosphor-green accent
+val TerminalBackground = Color(0xFF0A0E0C)
+val TerminalSurface = Color(0xFF121815)
+val TerminalSurfaceVariant = Color(0xFF1B2420)
+val TerminalGreen = Color(0xFF39FF88)
+val TerminalGreenDim = Color(0xFF2BB86A)
+val TerminalAmber = Color(0xFFFFB020) // used for spam/warning states
+val TerminalRed = Color(0xFFFF5C5C)
+val TerminalTextPrimary = Color(0xFFE3F5E8)
+val TerminalTextSecondary = Color(0xFF8FA69A)
